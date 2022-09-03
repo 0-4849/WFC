26f0f029494802cf7c70ae4bcea63631a3464ae2
0-4849 <tars.nijman@gmail.com>
2022-09-03 15:59:20 +0200

    ?

--- WFC.py
+++ WFC.py
@@ -1,55 +1,48 @@
 import random
 
-chars = (" ", "┴", "├", "┬", "┤")
-
-SIZE_X, SIZE_Y = 20,10
-LEFT, RIGHT, UP, DOWN, BLANK = 4, 2, 1, 3, 0
-
-rules = {
-    BLANK: [
-        [BLANK, UP],
-        [BLANK, RIGHT],
-        [BLANK, DOWN],
-        [BLANK, LEFT],
-    ],
-    UP: [
-        [RIGHT, LEFT, DOWN],
-        [LEFT, UP, DOWN],
-        [BLANK, DOWN],
-        [RIGHT, UP, DOWN],
-    ],
-    RIGHT: [
-        [RIGHT, LEFT, DOWN],
-        [LEFT, UP, DOWN],
-        [RIGHT, LEFT, UP],
-        [BLANK, LEFT],
-    ],
-    DOWN: [
-        [BLANK, UP],
-        [LEFT, UP, DOWN],
-        [RIGHT, LEFT, UP],
-        [RIGHT, UP, DOWN],
-    ],
-    LEFT: [
-        [RIGHT, LEFT, DOWN],
-        [BLANK, RIGHT],
-        [RIGHT, LEFT, UP],
-        [UP, DOWN, RIGHT],
-    ]
-}
-
-
 class Tile():
-    def __init__(self, id):
-        self.is_collapsed = False
-        self.options = [LEFT, RIGHT, UP, DOWN, BLANK]
-        self.id = id
+	def __init__(self, name, char, edges) -> None:
+		self.name = name		
+		self.char = char
+		self.edges = edges
+  
+		self.up = []
+		self.right = []
+		self.down = []
+		self.left = []
 
-    def __lt__(self, other):
-        return len(self.options) < len(other.options)
+class Cell():
+	def __init__(self, id) -> None:
+		self.is_collapsed = False
+		self.options = {0,1,2,3,4}
+		self.id = id
 
+	def __lt__(self, other):
+		return len(self.options) < len(other.options)
 
-grid = [Tile(i) for i in range(SIZE_X*SIZE_Y)]
+tiles = [0]*5
+tiles[0] = Tile(0, " ", [0,0,0,0])
+tiles[1] = Tile(1, "┴", [1,1,0,1])
+tiles[2] = Tile(2, "├", [1,1,1,0])
+tiles[3] = Tile(3, "┬", [0,1,1,1])
+tiles[4] = Tile(4, "┤", [1,0,1,1])
+
+chars = (" ", "┴", "├", "┬", "┤")
+SIZE_X, SIZE_Y = 20, 10
+BLANK, UP, RIGHT, DOWN, LEFT = 0, 1, 2, 3, 4
+# calculate compatabilities for edges
+rules = {}
+
+for i in tiles:
+	rules[i.name] = {}
+	for edge_index, edge in enumerate(i.edges):
+		rules[i.name].update({edge_index:set()})
+		for j in tiles:
+			if edge == j.edges[(edge_index + 2) % 4]:
+				rules[i.name][edge_index] |= {j.name}
+				
+
+grid = [Cell(i) for i in range(SIZE_X*SIZE_Y)]
 output = [" "]*SIZE_X*SIZE_Y
 
 while True:
@@ -63,7 +56,8 @@ while True:
 
 	current_tile = random.choice(possible_tiles)
 	current_tile.is_collapsed = True
-	current_tile.options = [random.choice(current_tile.options)]
+	random_key = random.choice(list(current_tile.options))
+	current_tile.options = set([random_key])
 
 	next_grid = grid.copy()
 
@@ -73,14 +67,14 @@ while True:
 			if grid[index].is_collapsed:
 				next_grid[index] = grid[index]
 			else:
-				next_options = {0, 1, 2, 3, 4}
+				next_options = {0,1,2,3,4}
 				
 				if y > 0:  # check up
 					up = grid[(y-1)*SIZE_X + x]
 					valid_options = set()
 					for option in up.options:
 						valid = rules[option][2]
-						valid_options = valid_options.union(set(valid))
+						valid_options = valid_options.union(valid)
 					next_options = next_options & valid_options
 
 				if x < SIZE_X - 1:  # check right
@@ -88,7 +82,7 @@ while True:
 					valid_options = set()
 					for option in right.options:
 						valid = rules[option][3]
-						valid_options = valid_options.union(set(valid))
+						valid_options = valid_options.union(valid)
 					next_options = next_options & valid_options
 
 				if y < SIZE_Y - 1:  # check down
@@ -96,7 +90,7 @@ while True:
 					valid_options = set()
 					for option in down.options:
 						valid = rules[option][0]
-						valid_options = valid_options.union(set(valid))
+						valid_options = valid_options.union(valid)
 					next_options = next_options & valid_options
 
 				if x > 0:  # check left
@@ -104,26 +98,19 @@ while True:
 					valid_options = set()
 					for option in left.options:
 						valid = rules[option][1]
-						valid_options = valid_options.union(set(valid))
+						valid_options = valid_options.union(valid)
 					next_options = next_options & valid_options
 						
 				
 
-				next_grid[index].options = list(next_options)
-				#char_list = [chars[i.options[0]] if len(i.options) == 1 else "█" for i in grid ]
-
-				#for i in range(0,len(char_list),SIZE_X+1): char_list.insert(i+SIZE_X, "\n")
-				#print("\033[2J","".join(char_list),sep="")
+				next_grid[index].options = next_options
     
     
     
-	grid = next_grid
+	grid = next_grid.copy()
 
-
-char_list = [chars[i.options[0]] for i in grid]
+char_list = [chars[list(i.options)[0]] for i in grid]
 
 for i in range(0,len(char_list),SIZE_X+1): char_list.insert(i+SIZE_X, "\n")
 
 print(*char_list, sep="")
-
-#comment to test git commit
